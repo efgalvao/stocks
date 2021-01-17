ceb02c41522d10b3ad3ef5c0494cc2f8ec9712f0
efgalvao <solfieri@hotmail.com>
2021-01-17 11:36:25 -0300

    WIP - Add some basic CRUD functions.

--- app/controllers/values_controller.rb
+++ app/controllers/values_controller.rb
@@ -1,15 +1,16 @@
 class ValuesController < ApplicationController
-  before_action :set_value, only: [:show, :edit, :update, :destroy]
+  before_action :set_value, only: %i[show edit update destroy]
 
   # GET /values
   # GET /values.json
   def index
-    @values = Value.all
+    @values = Value.where(facade_id: params[:facade_id])
   end
 
   # GET /values/1
   # GET /values/1.json
   def show
+    @value = Value.find(params[:id])
   end
 
   # GET /values/new
@@ -18,8 +19,7 @@ class ValuesController < ApplicationController
   end
 
   # GET /values/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /values
   # POST /values.json
@@ -28,7 +28,7 @@ class ValuesController < ApplicationController
 
     respond_to do |format|
       if @value.save
-        format.html { redirect_to @value, notice: 'Value was successfully created.' }
+        format.html { redirect_to facade_values_path(@facade), notice: 'Value was successfully created.' }
         format.json { render :show, status: :created, location: @value }
       else
         format.html { render :new }
@@ -62,13 +62,14 @@ class ValuesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_value
-      @value = Value.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def value_params
-      params.require(:value).permit(:date, :value, :facade_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_value
+    @value = Value.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def value_params
+    params.require(:value).permit(:date, :value, :facade_id)
+  end
 end
