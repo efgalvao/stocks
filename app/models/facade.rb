c93e4c93df337c2f66b7fba62faa38304dbe64c5
efgalvao <solfieri@hotmail.com>
2021-02-04 23:17:25 -0300

    Add dependend: destroy in facade model

--- app/models/facade.rb
+++ app/models/facade.rb
@@ -1,8 +1,8 @@
 class Facade < ApplicationRecord
-  has_many :dividends
+  has_many :dividends, dependent: :destroy
   accepts_nested_attributes_for :dividends, allow_destroy: true
-  has_many :stocks
+  has_many :stocks, dependent: :destroy
   accepts_nested_attributes_for :stocks, allow_destroy: true
-  has_many :values
+  has_many :values, dependent: :destroy
   accepts_nested_attributes_for :values, allow_destroy: true
 end
