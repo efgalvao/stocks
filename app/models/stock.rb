8b23b31a71ef537ceda5135ff7ecc0705d6c3295
efgalvao <solfieri@hotmail.com>
2021-02-14 16:16:26 -0300

    Add some data to #show.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,11 +1,13 @@
 class Stock < ApplicationRecord
   belongs_to :facade
 
+  # scope :average_price, -> { }
+
   def self.average_price
-    self.all.sum('aquisition_value')
+    self.all.average('aquisition_value')
   end
 
   def self.monthly_average_price
-    group_by_month(:aquisition_date, last: 12, current: true).average_price
+    b = group_by_month(:aquisition_date, last: 12, current: true).average_price
   end
 end
