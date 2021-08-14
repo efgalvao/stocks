9daaaf718cacc29f835b87724d5381ca88917ba9
efgalvao <solfieri@hotmail.com>
2021-08-14 14:11:37 -0300

    Create and add  last_balance helper

--- app/helpers/balances_helper.rb
+++ app/helpers/balances_helper.rb
@@ -1,2 +1,8 @@
 module BalancesHelper
+  def last_balance(account_id)
+    last_balance = Balance.where(account_id: account_id).order('date asc').first
+
+    Account.find(account_id).generate_balance if last_balance.nil? || last_balance.date + 1.day < DateTime.current
+    Account.find(account_id).balances.last.balance
+  end
 end
