94b933ed52f760c5d4b321c14d3c54832ac30f61
efgalvao <solfieri@hotmail.com>
2021-08-14 14:22:33 -0300

    Create sell and total_amount mehods for stock

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,10 +1,17 @@
 class Stock < ApplicationRecord
   has_many :dividends, dependent: :destroy
-  accepts_nested_attributes_for :dividends, allow_destroy: true
-  has_many :stocks, dependent: :destroy
-  accepts_nested_attributes_for :quotes, allow_destroy: true
+  has_many :quotes, dependent: :destroy
   has_many :values, dependent: :destroy
-  accepts_nested_attributes_for :values, allow_destroy: true
   belongs_to :account
 
+  def sell(amount, value)
+    total_value = amount * value
+    account.balance += total_value
+    quotes.first(amount).destroy
+  end
+
+  def total_amount
+    value = values.order('date').first&.value || quotes.order('aquisition_date desc').first.aquisition_value
+    value * quotes.count
+  end
 end
