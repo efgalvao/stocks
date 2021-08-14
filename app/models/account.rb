d5654480f9e83881d55f152850ef0378f3994d57
efgalvao <solfieri@hotmail.com>
2021-08-14 14:18:01 -0300

    Create generare_balance method for acount

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,10 @@
 class Account < ApplicationRecord
+  has_many :balances
+  has_many :stocks
+
+  def generate_balance
+    stocks
+    total = stocks.inject(0) { |sum, stock| stock.total_amount + sum }
+    balances.create(balance: total)
+  end
 end
