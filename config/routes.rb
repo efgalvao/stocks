558e02848b08299db07a108391ac671d8fe3d37b
efgalvao <solfieri@hotmail.com>
2021-08-17 15:23:00 -0300

    Routes refactored

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,61 @@
-#                 Prefix Verb   URI Pattern                                                                              Controller#Action
-#                   root GET      /                                                                                     stocks#main
-#       account_balances GET      /accounts/:account_id/balances(.:format)                                              balances#index
-#                        POST     /accounts/:account_id/balances(.:format)                                              balances#create
-#    new_account_balance GET      /accounts/:account_id/balances/new(.:format)                                          balances#new
-#   edit_account_balance GET      /accounts/:account_id/balances/:id/edit(.:format)                                     balances#edit
-#        account_balance GET      /accounts/:account_id/balances/:id(.:format)                                          balances#show
-#                        PATCH    /accounts/:account_id/balances/:id(.:format)                                          balances#update
-#                        PUT      /accounts/:account_id/balances/:id(.:format)                                          balances#update
-#                        DELETE   /accounts/:account_id/balances/:id(.:format)                                          balances#destroy
-#               accounts GET      /accounts(.:format)                                                                   accounts#index
-#                        POST     /accounts(.:format)                                                                   accounts#create
-#            new_account GET      /accounts/new(.:format)                                                               accounts#new
-#           edit_account GET      /accounts/:id/edit(.:format)                                                          accounts#edit
-#                account GET      /accounts/:id(.:format)                                                               accounts#show
-#                        PATCH    /accounts/:id(.:format)                                                               accounts#update
-#                        PUT      /accounts/:id(.:format)                                                               accounts#update
-#                        DELETE   /accounts/:id(.:format)                                                               accounts#destroy
-#        stock_dividends POST     /stocks/:stock_id/dividends(.:format)                                                 dividends#create
-#         stock_dividend DELETE   /stocks/:stock_id/dividends/:id(.:format)                                             dividends#destroy
-#           stock_values POST     /stocks/:stock_id/values(.:format)                                                    values#create
-#            stock_value DELETE   /stocks/:stock_id/values/:id(.:format)                                                values#destroy
-#                 stocks GET      /stocks(.:format)                                                                     stocks#index
-#                        POST     /stocks(.:format)                                                                     stocks#create
-#              new_stock GET      /stocks/new(.:format)                                                                 stocks#new
-#             edit_stock GET      /stocks/:id/edit(.:format)                                                            stocks#edit
-#                  stock GET      /stocks/:id(.:format)                                                                 stocks#show
-#                        PATCH    /stocks/:id(.:format)                                                                 stocks#update
-#                        PUT      /stocks/:id(.:format)                                                                 stocks#update
-#                        DELETE   /stocks/:id(.:format)                                                                 stocks#destroy
-#                        POST     /quotes(.:format)                                                                     quotes#create
-#              new_quote GET      /quotes/new(.:format)                                                                 quotes#new
-#             edit_quote GET      /quotes/:id/edit(.:format)                                                            quotes#edit
-#                  quote GET      /quotes/:id(.:format)                                                                 quotes#show
-#                        PATCH    /quotes/:id(.:format)                                                                 quotes#update
-#                        PUT      /quotes/:id(.:format)                                                                 quotes#update
-#                        DELETE   /quotes/:id(.:format)                                                                 quotes#destroy
-#              dividends GET      /dividends(.:format)                                                                  dividends#index
-#                        POST     /dividends(.:format)                                                                  dividends#create
-#           new_dividend GET      /dividends/new(.:format)                                                              dividends#new
-#          edit_dividend GET      /dividends/:id/edit(.:format)                                                         dividends#edit
-#               dividend GET      /dividends/:id(.:format)                                                              dividends#show
-#                        PATCH    /dividends/:id(.:format)                                                              dividends#update
-#                        PUT      /dividends/:id(.:format)                                                              dividends#update
-#                 values GET      /values(.:format)                                                                     values#index
-#                 charts GET      /charts(.:format)                                                                     charts#index
-#                        POST     /charts(.:format)                                                                     charts#create
-#              new_chart GET      /charts/new(.:format)                                                                 charts#new
-#             edit_chart GET      /charts/:id/edit(.:format)                                                            charts#edit
-#                  chart GET      /charts/:id(.:format)                                                                 charts#show
-#                        PATCH    /charts/:id(.:format)                                                                 charts#update
-#                        PUT      /charts/:id(.:format)                                                                 charts#update
-#                        DELETE   /charts/:id(.:format)                                                                 charts#destroy
+#                 Prefix Verb   URI Pattern                                                       Controller#Action
+#                    root GET    /                                                                stocks#main
+#        account_balances GET    /accounts/:account_id/balances(.:format)                         balances#index
+#                         POST   /accounts/:account_id/balances(.:format)                         balances#create
+#     new_account_balance GET    /accounts/:account_id/balances/new(.:format)                     balances#new
+#    edit_account_balance GET    /accounts/:account_id/balances/:id/edit(.:format)                balances#edit
+#         account_balance GET    /accounts/:account_id/balances/:id(.:format)                     balances#show
+#                         PATCH  /accounts/:account_id/balances/:id(.:format)                     balances#update
+#                         PUT    /accounts/:account_id/balances/:id(.:format)                     balances#update
+#                         DELETE /accounts/:account_id/balances/:id(.:format)                     balances#destroy
+#                accounts GET    /accounts(.:format)                                              accounts#index
+#                         POST   /accounts(.:format)                                              accounts#create
+#             new_account GET    /accounts/new(.:format)                                          accounts#new
+#            edit_account GET    /accounts/:id/edit(.:format)                                     accounts#edit
+#                 account GET    /accounts/:id(.:format)                                          accounts#show
+#                         PATCH  /accounts/:id(.:format)                                          accounts#update
+#                         PUT    /accounts/:id(.:format)                                          accounts#update
+#                         DELETE /accounts/:id(.:format)                                          accounts#destroy
+#          stock_dividend DELETE /stocks/:stock_id/dividends/:id(.:format)                        dividends#destroy
+#        edit_stock_value GET    /stocks/:stock_id/values/:id/edit(.:format)                      values#edit
+#             stock_value PATCH  /stocks/:stock_id/values/:id(.:format)                           values#update
+#                         PUT    /stocks/:stock_id/values/:id(.:format)                           values#update
+#                         DELETE /stocks/:stock_id/values/:id(.:format)                           values#destroy
+#                  stocks GET    /stocks(.:format)                                                stocks#index
+#                         POST   /stocks(.:format)                                                stocks#create
+#               new_stock GET    /stocks/new(.:format)                                            stocks#new
+#              edit_stock GET    /stocks/:id/edit(.:format)                                       stocks#edit
+#                   stock GET    /stocks/:id(.:format)                                            stocks#show
+#                         PATCH  /stocks/:id(.:format)                                            stocks#update
+#                         PUT    /stocks/:id(.:format)                                            stocks#update
+#                         DELETE /stocks/:id(.:format)                                            stocks#destroy
+#                  quotes GET    /quotes(.:format)                                                quotes#index
+#                         POST   /quotes(.:format)                                                quotes#create
+#               new_quote GET    /quotes/new(.:format)                                            quotes#new
+#              edit_quote GET    /quotes/:id/edit(.:format)                                       quotes#edit
+#                   quote GET    /quotes/:id(.:format)                                            quotes#show
+#                         PATCH  /quotes/:id(.:format)                                            quotes#update
+#                         PUT    /quotes/:id(.:format)                                            quotes#update
+#                         DELETE /quotes/:id(.:format)                                            quotes#destroy
+#               dividends GET    /dividends(.:format)                                             dividends#index
+#                         POST   /dividends(.:format)                                             dividends#create
+#            new_dividend GET    /dividends/new(.:format)                                         dividends#new
+#           edit_dividend GET    /dividends/:id/edit(.:format)                                    dividends#edit
+#                dividend GET    /dividends/:id(.:format)                                         dividends#show
+#                         PATCH  /dividends/:id(.:format)                                         dividends#update
+#                         PUT    /dividends/:id(.:format)                                         dividends#update
+#                  values GET    /values(.:format)                                                values#index
+#                         POST   /values(.:format)                                                values#create
+#               new_value GET    /values/new(.:format)                                            values#new
+#                   value GET    /values/:id(.:format)                                            values#show
+#                  charts GET    /charts(.:format)                                                charts#index
+#                         POST   /charts(.:format)                                                charts#create
+#               new_chart GET    /charts/new(.:format)                                            charts#new
+#              edit_chart GET    /charts/:id/edit(.:format)                                       charts#edit
+#                   chart GET    /charts/:id(.:format)                                            charts#show
+#                         PATCH  /charts/:id(.:format)                                            charts#update
+#                         PUT    /charts/:id(.:format)                                            charts#update
+#                         DELETE /charts/:id(.:format)                                            charts#destroy
 
 Rails.application.routes.draw do
   root 'stocks#main'
