d2a7b9907c438d2dfdc8b67ac38c50786fb71685
efgalvao <solfieri@hotmail.com>
2021-08-17 15:27:40 -0300

    Add dependent: :destroy in accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,10 +1,10 @@
 class Account < ApplicationRecord
-  has_many :balances
-  has_many :stocks
+  has_many :balances, dependent: :destroy
+  has_many :stocks, dependent: :destroy
 
   def generate_balance
     stocks
-    total = stocks.inject(0) { |sum, stock| stock.total_amount + sum }
+    total = stocks.inject(0) { |sum, stock| stock.updated_balance + sum }
     balances.create(balance: total)
   end
 end
