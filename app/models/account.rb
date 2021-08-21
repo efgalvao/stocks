d976152e58978e09ba71a06f9f9b77cb74234a24
efgalvao <solfieri@hotmail.com>
2021-08-21 12:09:18 -0300

    Create total_invested method for accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,9 +2,16 @@ class Account < ApplicationRecord
   has_many :balances, dependent: :destroy
   has_many :stocks, dependent: :destroy
 
+  # TODO: Find a way to retrieve the last balance if there were no changes
   def generate_balance
-    stocks
     total = stocks.inject(0) { |sum, stock| stock.updated_balance + sum }
     balances.create(balance: total)
+  end
+
+  def total_invested
+    total = 0
+    stocks.each do |stock|
+      total += stock.total_invested
+    end
   end
 end
