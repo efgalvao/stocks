3c5c886c0211c84584101221a787bdb94df6d4b9
efgalvao <solfieri@hotmail.com>
2021-08-22 12:21:02 -0300

    Create current_value and monthly_value for stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -25,14 +25,33 @@ class Stock < ApplicationRecord
     value * quotes.count
   end
   # TODO create a method to calculate balance of a specific month
-  def month_balance
-    value = if values.where(date:  desc').first.nil?
-              quotes.order('aquisition_date desc').first.aquisition_value
-            else
-              values.order('date desc').first&.value
-            end
+  # def month_balance
+  #   value = if values.where(date:  desc').first.nil?
+  #             quotes.order('aquisition_date desc').first.aquisition_value
+  #           else
+  #             values.order('date desc').first&.value
+  #           end
 
-    value * quotes.count
+  #   value * quotes.count
+  # end
+
+  # get the current value of stock
+  def current_value
+    if values.order('date desc').first&.value.nil?
+      quotes.order('aquisition_date desc').first&.aquisition_value
+    else
+      values.order('date desc').first&.value
+    end
   end
+
+  def monthly_value
+    if values.empty?
+      quotes.group_by_month(:aquisition_date, last: 12, current: true).average('aquisition_value')
+      # quotes.order('aquisition_date desc').first&.aquisition_value
+    else
+      values.group_by_month(:date, last: 12, current: true).average('value')
+    end
+end
+  
   
 end
