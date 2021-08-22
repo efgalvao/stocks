e15b5ab6991af505e42bfb59cf2eced147bc9da7
efgalvao <solfieri@hotmail.com>
2021-08-22 12:13:39 -0300

    Change last_balance helper

--- app/helpers/balances_helper.rb
+++ app/helpers/balances_helper.rb
@@ -1,9 +1,9 @@
 module BalancesHelper
   def last_balance(account_id)
-    last_balance = Balance.where(account_id: account_id).order('date asc').first
     account = Account.find(account_id)
 
-    account.generate_balance if last_balance.nil? || last_balance.created_at < account.updated_at
+    account.generate_balance if account.balances.order('date desc').first.nil? ||
+     last_balance.created_at < account.updated_at
     Account.find(account_id).balances.last.balance
   end
 end
