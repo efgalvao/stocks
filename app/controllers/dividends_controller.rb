1c97ba7ca2f83a15e559f0700e6487fad926c769
efgalvao <solfieri@hotmail.com>
2021-08-23 14:05:49 -0300

    Refactor in dividends controler

--- app/controllers/dividends_controller.rb
+++ app/controllers/dividends_controller.rb
@@ -1,17 +1,15 @@
 class DividendsController < ApplicationController
-  before_action :set_dividend, only: [:show, :edit, :update, :destroy]
+  before_action :set_dividend, only: %i[show edit update destroy]
 
   # GET /dividends
   # GET /dividends.json
   def index
-    @dividends = Dividend.all
+    @dividends = Dividend.all.order('date desc').limit(10)
   end
 
   # GET /dividends/1
   # GET /dividends/1.json
-  def show
-    @dividend
-  end
+  def show; end
 
   # GET /dividends/new
   def new
@@ -19,8 +17,7 @@ class DividendsController < ApplicationController
   end
 
   # GET /dividends/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /dividends
   # POST /dividends.json
@@ -63,13 +60,14 @@ class DividendsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_dividend
-      @dividend = Dividend.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def dividend_params
-      params.require(:dividend).permit(:date, :value, :stock_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_dividend
+    @dividend = Dividend.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def dividend_params
+    params.require(:dividend).permit(:date, :value, :stock_id)
+  end
 end
