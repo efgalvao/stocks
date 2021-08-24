9fcde43eef640997f56e0e2e5db5c3396be941d1
efgalvao <solfieri@hotmail.com>
2021-08-24 17:48:30 -0300

    Create helpers for accounts

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -8,4 +8,24 @@ module AccountsHelper
     end
     total
   end
+
+  def free_total
+    total = 0
+    Account.includes(:balances).savings_accounts.each do |account|
+      next if account.balances.newest_balance.blank?
+
+      total += account.balances.newest_balance.balance
+    end
+    total
+  end
+
+  def locked_total
+    total = 0
+    Account.includes(:balances).stocks_accounts.each do |account|
+      next if account.balances.newest_balance.blank?
+
+      total += account.balances.newest_balance.balance
+    end
+    total
+  end
 end
