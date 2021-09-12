21ba2c367aa6d386ebd686f7066f3f7470e1a9f9
efgalvao <solfieri@hotmail.com>
2021-09-12 14:57:28 -0300

    Change account helper

--- app/helpers/balances_helper.rb
+++ app/helpers/balances_helper.rb
@@ -2,8 +2,9 @@ module BalancesHelper
   def last_balance(account_id)
     account = Account.find(account_id)
 
-    account.generate_balance if account.balances.order('date desc').first.nil? ||
-                                account.balances.current.updated_at < account.updated_at
+    account.generate_balance if account.balances.current.first&.updated_at.nil? ||
+                                account.balances.current.first.updated_at < account.updated_at
+
     Account.find(account_id).balances.last.balance
   end
 end
