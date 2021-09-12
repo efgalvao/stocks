8ead33027db110533c7ef6d8f751c8fa2392da9a
efgalvao <solfieri@hotmail.com>
2021-09-12 15:10:39 -0300

    Fix some rubocop warnings

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { create(:user)}
+  let(:user) { create(:user) }
 
   it "is valid with valid attributes" do
     expect(user).to be_valid
   end
 
   it "is not valid without a name" do
-    subject.name = nil
-    expect(subject).to_not be_valid
+    user.name = nil
+    expect(user).not_to be_valid
   end
 end
